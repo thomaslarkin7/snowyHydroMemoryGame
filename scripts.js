11cbd996b25c392d71d771f43cf41d0ac0966c91
thomaslarkin7 <thomaslarkin4@gmail.com>
2019-05-31 11:18:21 +1000

    Memory Game Files initial upload

---
+++ scripts.js
@@ -0,0 +1,207 @@
+//Memory Game: T Larkin
+
+//Make a list of all memory card elements and store this inside a constant named 'cards'
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+//once two cards have been clicked we need to 'lock' the board to prevent further clicking
+let lockBoard = false;
+let firstCard, secondCard;
+let cardValue;
+
+//MODAL VARIABLES
+//Taken from this webpage https://sabe.io/tutorials/how-to-create-modal-popup-box
+var modal = document.querySelector(".modal");
+
+// Get the modal
+var modals = document.getElementsByClassName('modal');
+
+var imgBs = document.getElementsByClassName('imgB');
+
+//var trigger = document.querySelector(".trigger");
+var closeButtons = document.getElementsByClassName('close-button');
+var closeButton = document.querySelector(".close-button");
+
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+//Now loop through this list and attach an 'Event Listener' to each card. We are going to
+//listen for a 'click' event and when this fires, we will run the 'card flip' function
+//Begin function flipCard
+function flipCard(){
+    //if the lockBoard variable is true, do not execute the rest of the code 
+    if (lockBoard) return;
+    //to prevent the same card from being clicked twice and locking out the game
+    if(this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if(!hasFlippedCard){
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    } 
+    // second click
+    hasFlippedCard = false;
+    secondCard = this;
+    checkForMatch();
+
+}//end function flipCard
+
+function checkForMatch(){
+    //do the cards match
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    if (isMatch == true){
+        //console.log('We Have a Match');
+        //if the cards that are matched are the Australia cards, toggle the 'Australia' modal
+        //if (firstCard.dataset.framework == 'australia'){
+            cardValue = firstCard.dataset.framework;
+            if (cardValue == 'DLeckie'){setTimeout(toggleModalA, 1500);}
+            if (cardValue == 'PMeredith'){setTimeout(toggleModalV, 1500);}
+            if (cardValue == 'HJeromin'){setTimeout(toggleModalAn, 1500);}
+            if (cardValue == 'IBotka'){setTimeout(toggleModalE, 1500);}
+            if (cardValue == 'KSinkevicius'){setTimeout(toggleModalB, 1500);}
+            if (cardValue == 'AGlavica'){setTimeout(toggleModalR, 1500);}
+            if (cardValue == 'JPeska'){setTimeout(toggleModalH, 1500);}
+            if (cardValue == 'RLodge'){setTimeout(toggleModalAp, 1500);}
+            if (cardValue == 'GVanWezel'){setTimeout(toggleModalAu, 1500);}
+            if (cardValue == 'ABeranek'){setTimeout(toggleModalD, 1500);}
+
+        //}
+        //console.log(modals);
+    }
+
+    //ternary operator
+    isMatch ? disableCards() : unflipCards();
+}//end function checkForMatch
+
+
+console.log(imgBs);
+
+
+    //If the cards matched are the Australia Cards, toggle 'Australia' Modal
+
+
+function toggleModalA(){modals[0].classList.toggle("show-modal");}
+function toggleImgA(){imgBs[0].style.visibility = "visible";}
+
+function toggleModalV(){modals[1].classList.toggle("show-modal");}
+function toggleImgV(){imgBs[1].style.visibility = "visible";}
+
+function toggleModalAn(){modals[2].classList.toggle("show-modal");}
+function toggleImgAn(){imgBs[2].style.visibility = "visible";}
+
+function toggleModalE(){modals[3].classList.toggle("show-modal");}
+function toggleImgE(){imgBs[3].style.visibility = "visible";}
+
+function toggleModalB(){modals[4].classList.toggle("show-modal");}
+function toggleImgB(){imgBs[4].style.visibility = "visible";}
+
+function toggleModalR(){modals[5].classList.toggle("show-modal");}
+function toggleImgR(){imgBs[5].style.visibility = "visible";}
+
+function toggleModalH(){modals[6].classList.toggle("show-modal");}
+function toggleImgH(){imgBs[6].style.visibility = "visible";}
+
+function toggleModalAp(){modals[7].classList.toggle("show-modal");}
+function toggleImgAp() {imgBs[7].style.visibility = "visible";}
+
+function toggleModalAu(){modals[8].classList.toggle("show-modal");}
+function toggleImgAu(){imgBs[8].style.visibility = "visible";}
+
+function toggleModalD(){modals[9].classList.toggle("show-modal");}
+function toggleImgD(){imgBs[9].style.visibility = "visible";}
+
+
+function windowOnClick(event) {
+    if (event.target === modal && cardValue == 'DLeckie') {toggleModalA();}
+    if (event.target === modal && cardValue == 'PMeredith') {toggleModalV();}
+
+    if (event.target === modal && cardValue == 'HJeromin') {toggleModalAn();}
+
+    if (event.target === modal && cardValue == 'IBotka') {toggleModalE();}
+
+    if (event.target === modal && cardValue == 'KSinkevicius') {toggleModalB();}
+
+    if (event.target === modal && cardValue == 'AGlavica') {toggleModalR();}
+
+    if (event.target === modal && cardValue == 'JPeska') {toggleModalH();}
+    if (event.target === modal && cardValue == 'RLodge') {toggleModalAp();}
+    if (event.target === modal && cardValue == 'GVanWezel') {toggleModalAu();}
+    if (event.target === modal && cardValue == 'ABeranek') {toggleModalD();}
+}
+
+function disableCards(){
+    //it's a match
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+} //end function disableCards
+
+function unflipCards(){
+    lockBoard = true;
+
+    //not a match
+    //the numeral value in this setTimeout sets the amount of time for the 
+    //card to remain visible on screen before being flipped over again
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        resetBoard();
+    }, 1500);   
+} //end function unflipCards
+
+function resetBoard(){
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+} //end function resetBoard
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random()*12);
+        card.style.order = randomPos;
+    });
+})();
+
+function off(){
+    document.getElementById("loadOverlay").style.display = "none";
+    console.log("Pressed Right Here");
+}
+//these extra parenthesis at the end make this function an immediately 
+//invoked function expression
+
+//MODAL LISTENERS
+//TAKEN FROM this webpage https://sabe.io/tutorials/how-to-create-modal-popup-box
+//trigger.addEventListener("click", toggleModal);
+closeButtons[0].addEventListener("click", toggleModalA); //Close Button A
+closeButtons[0].addEventListener("click", toggleImgA); //Img A to appear
+
+closeButtons[1].addEventListener("click", toggleModalV); //Close Button V
+closeButtons[1].addEventListener("click", toggleImgV); //Img V to appear
+
+closeButtons[2].addEventListener("click", toggleModalAn); //Close Button An
+closeButtons[2].addEventListener("click", toggleImgAn); //Image An to appear
+
+closeButtons[3].addEventListener("click", toggleModalE); //Close Button E
+closeButtons[3].addEventListener("click", toggleImgE); //Img E to appear
+
+closeButtons[4].addEventListener("click", toggleModalB); //Close Button B
+closeButtons[4].addEventListener("click", toggleImgB); //Img B to appear
+
+closeButtons[5].addEventListener("click", toggleModalR); //Close Button R
+closeButtons[5].addEventListener("click", toggleImgR); //Img R to appear
+
+closeButtons[6].addEventListener("click", toggleModalH); //Close Button H
+closeButtons[6].addEventListener("click", toggleImgH); //Img H to appear
+
+closeButtons[7].addEventListener("click", toggleModalAp); //Close Button Ap
+closeButtons[7].addEventListener("click", toggleImgAp); //Img Ap to appear
+
+closeButtons[8].addEventListener("click", toggleModalAu); //Close Button Au
+closeButtons[8].addEventListener("click", toggleImgAu); //Img R to appear Au
+
+closeButtons[9].addEventListener("click", toggleModalD); //Close Button D
+closeButtons[9].addEventListener("click", toggleImgD); //Img D to appear
+
+window.addEventListener("click", windowOnClick);
---
+++ sketch.js
@@ -0,0 +1,258 @@
+var canvas;
+var mic;
+
+var rainButton;
+var shineButton;
+var dayToNight;
+var dayTN = 0;
+var speed;
+var opacity = 0;
+var rainDrops = [];
+var xpos = 0;
+var ypos = 0;
+var starStep = 10;
+var sunY;
+var sunX;
+var sunSet = 0;
+var lastLightOpac;
+var noiseScale1=0.009;
+var noiseScale2=0.015;
+var noiseScale3=0.03;
+var range1 = 120;
+var range2 = 100;
+var range3 = 80;
+var rectH = 35;
+var powerArray = [];
+var curveDeg = 100;
+var plShift = 6;
+
+function windowResized() {
+  //console.log('resized');
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+  }
+
+  function draw() {
+    //let myColor = '#ffd363'
+    //background(myColor);
+    if (dayTN == 239){
+        speed = -0.25;
+      }
+    if (dayTN == 0){
+        speed = 0.25
+      }
+    
+    //Uncomment the line below for movement
+    dayTN = dayTN + speed;
+    if (dayTN < 241){
+    background(120,180,255);
+    sun(sunX,sunY);
+    lastLight();
+    drawMounts(210, range3, 90,  noiseScale3);
+    drawMounts(250, range2, 95,  noiseScale2);
+    drawMounts(320, range1, 100, noiseScale1);
+    //drawMountsFront(320, range1, 100, noiseScale1);
+    fill(1, 24, 63); 
+    rect(0, windowHeight-88, windowWidth, 88); 
+    dayNight();
+    opacity = dayTN;
+    sunY = dayTN*2 + 40;
+    sunX = dayTN/2 + 40;
+    if (dayTN > 25) {
+      sunSet = dayTN/2;
+    }  else {
+      sunSet = 0;
+    }
+    if (dayTN > 35) {
+      lastLightOpac = dayTN;	
+    } else if (dayTN > 175) {
+      lastLightOpac = 350 - dayTN;
+    }
+    else {
+      lastLightOpac = 0;
+    }
+
+    //Make some stars appear
+    if (dayTN > 110){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/110)*255)-255);
+      ellipse(windowWidth/4, 15, 10, 10);
+    }
+
+    if (dayTN > 120){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/120)*255)-255);
+      ellipse(windowWidth/14, 200, 7, 7);
+    }
+
+    if (dayTN > 130){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/130)*255)-255);
+      ellipse(windowWidth/6.5, 105, 5, 5);
+    }
+
+    if (dayTN > 140){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/140)*255)-255);
+      ellipse(windowWidth/7, 40, 8, 8);
+    }
+
+    if (dayTN > 150){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/150)*255)-255);
+      ellipse(windowWidth/2 + windowWidth/3, 200, 5, 5);
+    }
+
+    if (dayTN > 110){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/110)*255)-255);
+      ellipse(windowWidth/2 + windowWidth/5, 40, 6, 6);
+    }
+
+    if (dayTN > 120){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/120)*255)-255);
+      ellipse(windowWidth/2 - windowWidth/3, 50, 8, 8);
+    }
+
+    if (dayTN > 130){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/130)*255)-255);
+      ellipse(windowWidth/2 - windowWidth/3.8, 90, 3, 3);
+    }
+
+    if (dayTN > 140){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/140)*255)-255);
+      ellipse(windowWidth/2 - windowWidth/25, 30, 5, 5);
+    }
+
+    if (dayTN > 150){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/150)*255)-255);
+      ellipse(windowWidth/2 - windowWidth/15, 130, 7, 7);
+    }
+
+    if (dayTN > 110){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/110)*255)-255);
+      ellipse(windowWidth/2 + windowWidth/2.5, 40, 8, 8);
+    }
+
+    if (dayTN > 120){
+      noStroke();
+      fill(255, 255, 255, ((dayTN/120)*255)-255);
+      ellipse(windowWidth/2 + windowWidth/2.2, 200, 9, 9);
+    }
+    
+    }    
+  }
+
+
+  function drawMounts(peak,range,scale,noiseScale) {
+    for (var x=0; x < windowWidth; x++) {
+      var noiseVal = noise((200+x)*noiseScale, 100*noiseScale);
+      stroke(60,range,90);
+      line(x, (peak)+noiseVal*scale, x, windowHeight);
+    }
+  }
+
+  function drawMountsFront(peak,range,scale,noiseScale) {
+    for (var x=0; x < windowWidth+50; x++) {
+      var noiseVal = noise((200+x)*noiseScale, 100*noiseScale);
+      stroke(60,range,90);
+      line(x, (peak)+noiseVal*scale, x, windowHeight);
+      if (x%100==0){
+          strokeWeight(4);
+          stroke(109, 60, 3);
+          line(x, (peak)+noiseVal*scale - rectH, x, (peak)+noiseVal*scale);
+          line(x+6, (peak)+noiseVal*scale - rectH, x+6, (peak)+noiseVal*scale);
+        
+          line(x-3, (peak)+noiseVal*scale - rectH, x+9, (peak)+noiseVal*scale - rectH);
+  
+          powerArray.push([x+9, (peak)+noiseVal*scale - rectH]);
+          }
+    }
+
+    for (var j = 1; j<powerArray.length; j++){
+      strokeWeight(3);
+      //line(powerArray[j-1][0], powerArray[j-1][
+      noFill();
+      stroke(170, 144, 114);
+      //stroke(255, 0, 0);
+      curve(powerArray[j-1][0]-curveDeg, powerArray[j-1][1]-curveDeg,powerArray[j-1][0]-plShift, powerArray[j-1][1], powerArray[j][0]-plShift, powerArray[j][1], powerArray[j][0]+curveDeg, powerArray[j][1]-curveDeg);
+      noFill();
+      //bezier(x1, y1, x2, y2, x3, y3, x4, y4) 
+    }  
+   // print(powerArray);
+  }
+  
+  //function ellipsePulse(){
+  //    var pulseArray = [1, 0, 0, 0, 0, 0, 0, 0]
+  //    for (var k = 1; k<powerArray.length; k++){
+      //strokeWeight(3);
+      //
+  //      noStroke();
+  //      fill(232, 228, 11);
+      
+  //     ellipse(powerArray[k-1][0]-6, powerArray[k-1][1]-6, 10, 10);
+      
+      //bezier(x1, y1, x2, y2, x3, y3, x4, y4) 
+   // }
+  //}
+  
+
+
+  //------------------JS IMPORTED CODE HERE
+
+
+
+function dayNight() {
+  fill(10,20,40, opacity);
+	rect(0,0,windowWidth,windowHeight);
+}
+
+function stars() {
+	fill(225);
+  ellipse(xPos,yPos,diameter,diameter);
+}
+
+function greyDay() {
+  if (dayToNight.value < 190) {
+	opacity = dayToNight.value() + 20;
+  } 
+}
+
+function clearDay() {
+	background(120,180,255);
+}	
+
+function sun(sunX,sunY) {
+	push();
+  translate(sunX,sunY);
+  noStroke();
+  noStroke();
+  ellipse(0,0,20,20);
+  for (i = 0; i < 350; i++) {
+    fill(255,159,128,sunSet*10/i);
+  	ellipse(0,0,20+i,20+i);
+  }
+	fill(255,250,205);
+  ellipse(0,0,20,20);
+  pop();
+}
+
+function lastLight() {
+	for (i = 0; i < 350; i++) {
+    stroke(255,230,185,lastLightOpac*50/i);
+  	line(0,windowHeight-100-i,windowWidth,windowHeight-100-i);
+  }
+}
+
+
+
